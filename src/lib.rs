f91af60d6f6e400d41dd4ad3bc6aff582c95c84c
Tzian <1164336+Tzian@users.noreply.github.com>
2022-03-10 08:49:27 +0000

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,15 @@
+
+
+
+enum Action
+{
+    Push,
+    RemoveAt(usize),
+    Insert(usize),
+}
+
+
+
+
+let FirstVec = vec!["a", "b", "c"];
+let SecondVec = vec![(2, "cat"), (0, "dog"), (1, "mouse")];
